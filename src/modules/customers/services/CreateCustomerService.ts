5f7166ecb475df8af8f661cfed13c3a5826bad08
JamilsonMello <jamilsonmelow@gmail.com>
2020-08-23 19:32:13 -0300

    add register user

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomerRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const isUser = await this.customersRepository.findByEmail(email);
+
+    if (isUser) {
+      throw new AppError('User already exists', 401);
+    }
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
